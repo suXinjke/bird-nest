9caa55bb1cd7ead7a107d806ede77239a2d3b494
suXin <suxinjke@gmail.com>
2023-08-15 17:19:22 +0700

    add svelte

---
+++ svelte/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let numbers: number[] = []
+
+  function onClick() {
+    numbers.push(Math.random())
+    numbers = numbers
+  }
+</script>
+
+<div>
+  <h1>Svelte</h1>
+
+  <button on:click={onClick}>Add random number</button>
+
+  {#each numbers as number}
+    <div>
+      {number}
+    </div>
+  {/each}
+</div>
---
+++ svelte/index.ts
@@ -0,0 +1,6 @@
+import App from './App.svelte'
+import '../serve/style.css'
+
+new App({
+  target: document.getElementById('root')!,
+})
